216f4f84499ef0cfab4463af3edfa43f2842064a
oussamalkd <oussamalkd99@gmail.com>
2023-02-23 21:00:35 +0100

    authntification middlewawre

--- controllers/main.js
+++ controllers/main.js
@@ -1,6 +1,7 @@
-require("dotenv")
 const jwt = require("jsonwebtoken")
 const CustomAPIError = require("../errors/custom-error")
+
+
 const login = async (req, res) => {
     const { username, password } = req.body
     if(!username || !password) {
@@ -14,20 +15,9 @@ const login = async (req, res) => {
 }
 
 const dashboard = async (req, res) => {
-    const authHeader = req.headers.authorization
-    if(!authHeader || !authHeader.startsWith("Bearer ")) {
-        throw new CustomAPIError("the token not valid", 401)
-    }
-
-    const token = authHeader.split(' ')[1]
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        const random = Math.floor(Math.random()*100)
-        res.status(200).json({ msg: `Hello, ${decoded.username}`, secret: `Here is your authorized data, your numbebr is ${random}`})
-    } catch (error) {
-        throw new CustomAPIError("Unauthorized to access", 401)
-    }
-    
+    console.log(req.user)
+    const random = Math.floor(Math.random()*100)
+    res.status(200).json({ msg: `Hello, ${req.user.username}`, secret: `Here is your authorized data, your numbebr is ${random}`}) 
 }
 
 module.exports = { login, dashboard }
